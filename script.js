c6e8ec32f8ba02b0e4a12068ced2ceae74169568
Anna Kurochkina <basia220586@gmail.com>
2022-10-06 22:03:39 +0100

    State object, more test images, reset button, shuffle

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
-const cardImages = [
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
-    "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg"
-];
+import shuffleArray from "./helpers.js";
+
+const state = {
+    // gameStarted: false,
+    peakedCards: [],
+    flippedCards: [],
+    remainingFlips: 100,
+    remainingTime: 60,
+    cardImages: [
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://w7.pngwing.com/pngs/940/723/png-transparent-drawing-monster-cartoon-little-monsters-cartoon-character-photography-smiley-thumbnail.png",
+        "https://c8.alamy.com/comp/T9CB60/happy-cartoon-monster-vector-blue-monster-illustration-halloween-design-T9CB60.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg",
+        "https://c8.alamy.com/zooms/9/5616233966f24ab2a8ddfa37658c4ac7/t8tbmn.jpg"
+    ]
+};
 
 const renderGrid = () => {
     const grid = document.querySelector(".grid-container__memoryGame");
     grid.innerHTML = "";
-    cardImages.forEach((cardImage) => {
+    state.cardImages.forEach((cardImage) => {
         const card = document.createElement("div");
         card.className = "flip-card-container";
         card.innerHTML =
         `<div class="flip-card">
-            <div class="flip-card-front">Front</div>
+            <div class="flip-card-front"></div>
             <div class="flip-card-back">
                 <img src="${cardImage}" alt="Monster">
             </div>
@@ -46,4 +55,16 @@ const renderGrid = () => {
     });
 };
 
+shuffleArray(state.cardImages);
 renderGrid();
+
+const resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", () => {
+    state.peakedCards = [];
+    state.flippedCards = [];
+    state.remainingFlips = 100;
+    state.remainingTime = 60;
+    shuffleArray(state.cardImages);
+    renderGrid();
+});
